795411f424c51d5f0caf47b49a625a266a4c8788
Barış <51880864+wioenena-q@users.noreply.github.com>
2022-04-28 12:57:08 +0300

    fix: ignore whitespaces or empty string

--- src/nimdotenv.nim
+++ src/nimdotenv.nim
@@ -1,11 +1,11 @@
 from os import getAppDir, DirSep, putEnv
-from strutils import split, startsWith
+from strutils import split, startsWith, isEmptyOrWhitespace
 
 proc load*(filepath: string = getAppDir() & DirSep & ".env"): void =
   let content = readFile(filepath)
   let lines = split(content, "\n")
   for line in lines:
-    if startsWith(line, "#"):
+    if startsWith(line, "#") or isEmptyOrWhiteSpace(line):
       continue
     let keyVal = split(line, "=")
     putEnv(keyVal[0], keyVal[1])
